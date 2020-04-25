2f704751063f1d65878bc607c0f60edf0dab4733
crashtack <crashtack@gmail.com>
2020-04-25 18:39:11 +0100

    dash formating

--- dash_test/app.py
+++ dash_test/app.py
@@ -5,24 +5,43 @@ import dash_html_components as html
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+colors = {
+    'background': '#111111',
+    'text': '7FDBFF'
+}
+
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    html.H1(
+        children='Hello Dave',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
 
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
+    html.Div(
+        children='''Dash: A web application framework for Python.''',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
 
     dcc.Graph(
-        id='example-graph',
+        id='example-graph-2',
         figure={
             'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [4, 8, 2], 'type': 'bar', 'name': 'SF'},
                 {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
             ],
             'layout': {
-                'title': 'Dash Data Visualization'
+                'plot_bgcolor': colors['background'],
+                'paper_bgcolor': colors['background'],
+                'fonts': {
+                    'color': colors['text']
+                }
             }
         }
     )
