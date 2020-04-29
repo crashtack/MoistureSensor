7a90d56461fc1529f29c4059e57d2bad16728488
David Banks <crashtack@gmail.com>
2020-04-28 21:30:25 -0700

    Adding a test callback

--- plotly_test/app.py
+++ plotly_test/app.py
@@ -53,11 +53,19 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
             'color': colors['text']
         }
     ),
+    dcc.Input(id='my-id', value='initial value', type='text'),
+    html.Div(id='my-div'),
 
     dcc.Graph(figure=fig1),
     dcc.Graph(figure=fig2)
-
 ])
+
+@app.callback(
+    Output(component_id='my-div', component_property='children'),
+    [Input(component_id='my-id', component_property='value')]
+)
+def update_output_div(input_value):
+    return 'You\'ve entered "{}"'.format(input_value)
 
 if __name__ == '__main__':
     app.run_server(debug=True, host='0.0.0.0')
